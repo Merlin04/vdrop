fe68912bcf097598e79d54a74cc61b5018a002d3
merlin04 <Merlin04@users.noreply.github.com>
2021-07-19 17:59:09 -0500

    Add fonts

--- src/date.ts
+++ src/date.ts
@@ -1,6 +1,6 @@
 // Number representing July 19, 2021 at 0:00 UTC
 // For some reason months start at 0, not 1
-export const START_DATE = Date.UTC(2021, 6, 19, 4, 0, 0, 0);
+export const START_DATE = Date.UTC(2021, 6, 19, 6, 0, 0, 0);
 
 export function getDayNumber() {
     return (Date.now() - START_DATE) / MS_IN_DAY;
